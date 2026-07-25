a2e37c6d18ebf9d4219ffa14ca40587aaa2a6474
andrewojak1618-debug <andrewojak1618@gmail.com>
2026-07-25 16:35:27 +0200

    feat: configure responsive game canvas

--- classes/core/scenes/menu-scene.class.js
+++ classes/core/scenes/menu-scene.class.js
@@ -1,8 +1,18 @@
-import Phaser from 'phaser';
-import { SCENES } from '../../../js/config/game-config.js';
+import Phaser from "phaser";
+import { SCENES } from "../../../js/config/game-config.js";
 
 export class MenuScene extends Phaser.Scene {
   constructor() {
     super(SCENES.menu);
+  }
+
+  create() {
+    this.add
+      .text(360, 240, "BULLDOG: DARK CITY", {
+        fontFamily: "Arial",
+        fontSize: "32px",
+        color: "#ffffff",
+      })
+      .setOrigin(0.5);
   }
 }
