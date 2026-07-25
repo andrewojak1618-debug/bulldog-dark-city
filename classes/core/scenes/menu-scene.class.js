78c89af69e1fd53d430656fe6509570258422ec0
andrewojak1618-debug <andrewojak1618@gmail.com>
2026-07-25 18:12:20 +0200

    feat: add menu background and icon assets

--- classes/core/scenes/menu-scene.class.js
+++ classes/core/scenes/menu-scene.class.js
@@ -1,14 +1,27 @@
 import Phaser from "phaser";
 import { SCENES } from "../../../js/config/game-config.js";
 
+const MENU_BACKGROUND_KEY = "menu-background";
+const MENU_BACKGROUND_PATH = "/img/backgrounds/menu-background.png";
+
 export class MenuScene extends Phaser.Scene {
   constructor() {
     super(SCENES.menu);
   }
 
+  preload() {
+    this.load.image(MENU_BACKGROUND_KEY, MENU_BACKGROUND_PATH);
+  }
+
   create() {
+    const { width, height } = this.scale;
+
     this.add
-      .text(360, 240, "BULLDOG: DARK CITY", {
+      .image(width / 2, height / 2, MENU_BACKGROUND_KEY)
+      .setDisplaySize(width, height);
+
+    this.add
+      .text(width / 2, height / 2, "BULLDOG: DARK CITY", {
         fontFamily: "Arial",
         fontSize: "32px",
         color: "#ffffff",
